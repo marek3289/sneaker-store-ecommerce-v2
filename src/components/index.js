a772354e1e96b2109ff683f1abdc0bce4ba77c14
marek3289 <marek2494@o2.pl>
2020-05-09 00:54:01 +0200

    f

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as SEO } from './SEO';
-export { default as Header } from './Header';
+export { default as SEO } from './seo';
+export { default as Header } from './header';
 export { default as Footer } from './Footer';
 export { default as Skus } from './Skus';
 export { default as Preview } from './Preview';
