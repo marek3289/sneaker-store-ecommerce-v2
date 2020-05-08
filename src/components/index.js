0ba40624d39e9bcf3a60ac9d8d0d9e6abc547556
marek3289 <marek2494@o2.pl>
2020-05-09 00:41:12 +0200

    fixed bugs

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Layout } from './Layout';
+export { default as Layout } from './layout';
 export { default as SEO } from './SEO';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
