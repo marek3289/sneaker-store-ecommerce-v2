2a25ed938a19abf560e0b401b002f161046ce3c8
marek3289 <marek2494@o2.pl>
2020-05-18 17:29:20 +0200

    edit payment intent function

--- functions/payment_intent.js
+++ functions/payment_intent.js
@@ -1,39 +1,39 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-// exports.handler = async (req, res) => {
-//     try {
-//         const { amount } = req.body;
-
-//         const paymentIntent = await stripe.paymentIntents.create({
-//             amount,
-//             currency: "usd"
-//         });
-
-//         return {
-//             statusCode: 200,
-//             body: paymentIntent.client_secret
-//         }
-//     } catch (err) {
-//         return { statusCode: 500, body: err.toString() };
-//     }
-// }
-
 exports.handler = async (req, res) => {
-    if (req.method === 'POST') {
+    try {
         const { amount } = req.body;
 
-        try {
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount,
-                currency: "usd"
-            });
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "usd"
+        });
 
-            res.status(200).json(paymentIntent)
-        } catch (err) {
-            res.status(500).json({ statusCode: 500, message: err.message })
+        return {
+            statusCode: 200,
+            body: paymentIntent.client_secret
         }
-    } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() };
     }
 }
+
+// exports.handler = async (req, res) => {
+//     if (req.method === 'POST') {
+//         const { amount } = req.body;
+
+//         try {
+//             const paymentIntent = await stripe.paymentIntents.create({
+//                 amount,
+//                 currency: "usd"
+//             });
+
+//             res.status(200).json(paymentIntent)
+//         } catch (err) {
+//             res.status(500).json({ statusCode: 500, message: err.message })
+//         }
+//     } else {
+//         res.setHeader('Allow', 'POST');
+//         res.status(405).end('Method Not Allowed');
+//     }
+// }
